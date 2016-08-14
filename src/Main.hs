de588041a68f952cd14b40d683748399343647d5
Joe <notwhereitype@gmail.com>
2016-08-14 19:20:10 +0100

    Successfully parses edges & arcs including any attributes, returning a graph structure.

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,27 @@
-import qualified Text.Parsec as Parsec
+import qualified Text.Parsec as P
 import Text.Parsec ((<?>))
 import Text.Parsec ((<|>))
 import Data.Typeable
 import Data.Either
+import System.IO
 
 main :: IO ()
 main = do
-  x <- getLine
-  let y  = Parsec.parse findGraph "source" x
+  let path = "C:\\Users\\testuserignore\\github\\has\\gmlp\\small.graphml.xml"
+
+  x <- readFile path
+  -- x <- getLine
+  let y  = P.parse findGraph path x
   putStrLn $ show y
+  -- putStrLn $ show attributes y
+  -- putStrLn $ show nodes y
+  -- putStrLn $ show arcs y
+
+-- tellGraph :: (Show a) => Graph [(String, String)] [Node] [Arc] a -> IO()
+-- tellGraph (Graph {attributes = a, nodes = n arcs = v}) =do
+--   putStrLn a
+--   putStrLn n
+--   putStrLn v
 
 --Graphs take the form graph attributes, list of nodes, list of (tupled) arcs.
 data Arc = Arc [(String, String)] deriving (Show)
@@ -21,63 +34,67 @@ data Graph = Graph  {   attributes::    [(String,String)]
 
 
 -- This function links together finding of the graph then parsing its nodes and arcs
-findGraph:: Parsec.Parsec String () Graph
+findGraph:: P.Parsec String () Graph
 findGraph=do
-  _ <- Parsec.manyTill Parsec.anyChar (Parsec.try (Parsec.string "<graph "))
+  _ <-P.string "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+  _ <- (P.string "<graphml")
+  -- fileAtt <- P.manyTill attribute (P.char '>')
+  _ <- P.manyTill P.anyChar (P.try (P.string "<graph"))
   graph <- graphTag <?> "graph tag"
-  tags <- Parsec.many $ Parsec.choice [edgeTag, nodeTag]
-  endTag <?> "closing graph tag: \"</graph>\""
+  tags <- P.manyTill (((P.try nodeTag) <|> (P.try edgeTag)) <?> "edge or arc tag") (P.try endTag)
   let (arcs, nodes) = partitionEithers tags
   return $ Graph graph nodes arcs
+  -- <?> "closing graph tag: \"</graph>\""
 
 --------Tag Parsers---------
 
-graphTag:: Parsec.Parsec String () [(String,String)]
+graphTag:: P.Parsec String () [(String,String)]
 graphTag=do
-  -- Parsec.spaces
-  -- _ <- Parsec.string "<graph"
-  xs <- multiAttribute
-  _ <- Parsec.string "/>"
+  -- P.spaces
+  -- _ <- P.string "<graph"
+  xs <-P.manyTill attribute (P.string ">\n")
   return xs
 
-nodeTag:: Parsec.Parsec String () Tag --[(String, String)]
+nodeTag :: P.Parsec String () Tag --[(String, String)]
 nodeTag=do
-  Parsec.spaces
-  _ <- Parsec.string "<node"
-  xs <- multiAttribute
-  _ <- Parsec.string "\">"
+  P.spaces
+  _ <- P.string "<node"
+  xs <- tagParse
   return $ Right $ Node xs
 
-edgeTag:: Parsec.Parsec String () Tag --[(String, String)]
+edgeTag:: P.Parsec String () Tag --[(String, String)]
 edgeTag=do
-  Parsec.spaces
-  _ <- Parsec.string "<edge"
-  xs <- multiAttribute
-  _ <- Parsec.string "\">"
+  P.spaces
+  _ <- P.string "<edge"
+  xs <- tagParse
   return $ Left $ Arc xs
 
-endTag :: Parsec.Parsec String () ()
+endTag :: P.Parsec String () ()
 endTag = do
-  Parsec.spaces
-  _ <- Parsec.string "</graph>"
+  P.spaces
+  _ <- P.string "</graph>\n" <?> "graph closing tag"
   return ()
 
 ----------Auxillary functions----------
 
---returns a tupled list of a tags attirubtes.
-multiAttribute:: Parsec.Parsec String () [(String, String)]
-multiAttribute=Parsec.sepBy attribute Parsec.spaces
+-- --returns a tupled list of a tags attirubtes.
+-- multiAttribute::P.Parsec String () [(String, String)]
+-- multiAttribute=P.sepBy attribute P.spaces
+
+tagParse::P.Parsec String () [(String, String)]
+tagParse = P.manyTill attribute (P.string "/>\n")
 
 --parses an attribute from a tag
-attribute::Parsec.Parsec String () (String, String)
-attribute=do
-  x <- Parsec.many1 Parsec.alphaNum
-  _ <- Parsec.string "=\""
-  y <- Parsec.many1 Parsec.alphaNum
-  Parsec.char '\"'
+attribute::P.Parsec String () (String, String)
+attribute=P.try $ do
+  P.spaces
+  x <- P.many1 P.alphaNum
+  _ <- P.string "=\""
+  y <- P.many1 P.alphaNum
+  _ <- P.char '"'
   return (x, y)
 
-alphaNum::Parsec.Parsec String () String
-alphaNum=Parsec.many $ do
-  xs <- (Parsec.letter <|> Parsec.digit)
-  return xs
+-- alphaNum::P.Parsec String () String
+-- alphaNum=P.many $ do
+--   xs <- (P.letter <|> P.digit)
+--   return xs
