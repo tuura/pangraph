e87bc6dbbde90ba00b77c1150307d160e31bbe1c
Joe <notwhereitype@gmail.com>
2016-08-26 18:58:57 +0100

    I don't know what this does so I commit it anyway

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,29 @@
 module Main where
 
-import Data.List
-import Text.Parsec
+--Hackage/Base
 import System.IO
+import Text.Parsec
+import Data.Either
+
+--My modules
 import Gmlp
--- import Shorten
+import Types
+import Shorten
 
 main :: IO ()
 main = do
   putStrLn "File name:"
   file <- getLine
-  let path ="graphs/" ++ file ++ ".graphml.xml"
+  let path = file ++ ".graphml"
   x <- readFile path
-  let y = parse parseFile path x
-  putStrLn $ show y
+  -- let z = [] :: (Either a File)
+  let z = parse parseFile path x
+  putStrLn $ show z
+  putStrLn $ show (shortenFile (head (rights [z])))
   putStrLn "complete"
+
+-- printShortGraph:: File -> IO()
+-- printShortGraph x= putStrLn show $ shortenFile x
+--
+-- printParseErr:: ParseError -> IO()
+-- printParseErr _ = putStrLn "parse err"
--- src/Shorten.hs
+++ src/Shorten.hs
@@ -1,40 +1,29 @@
 module Shorten
-(  Node(..)
-,  Edge(..)
-,  ShortGraph(..)
-,  ShortFile(..)
-,  shortenFile
+(  shortenFile
 )where
 
 import Gmlp
+import Types
 import Data.List
 
-data Node = Node [Attribute] deriving (Show)
-data Edge = Edge [Attribute] deriving (Show)
-data ShortGraph = ShortGraph [Node] [Edge] deriving (Show)
-data ShortFile = ShortFile [ShortGraph] deriving (Show)
-
 shortenFile:: File -> ShortFile
-shortenFile (File _ _ ts) = ShortFile $ map shortenGraph (filter isGraph ts)
-  where isGraph (Tag str _ _ ) = (str == "graph")
+shortenFile (File _ _ []) = ShortFile []
+shortenFile (File _ _ ts) =ShortFile $ map (shortenGraph) (map filterGraphs $ filter isGraph ts)
+  where isGraph (Tag str _ _)=str `elem` ["graph", "graphml"]
+  
+filterGraphs:: Tag -> Tag
+filterGraphs tg@(Tag "graph" as ts) = tg
+filterGraphs (Tag "graphml" as ts) = head $ map filterGraphs ts
 
 shortenGraph:: Tag -> ShortGraph
-shortenGraph (Tag _ _ ts) = do
-  (ns, ts2) <- partition (\(Tag str _ _) -> str == "node") ts
-  (as, _) <- partition (\(Tag str _ _) -> str == "edge") ts2
-  return ShortGraph $ (shortenNode ns) (shortenEdge as)
+shortenGraph (Tag _ _ ts) = ShortGraph nsf esf
+  where
+    (ts2, _) = partition (\(Tag str _ _) -> str `elem` ["node" , "edge"]) ts
+    (ns, es) = partition (\(Tag str _ _) -> str == "node") $ map filterAtt ts2
+    nsf = map (\(Tag _ a _) -> (Node a)) ns
+    esf = map (\(Tag _ a _) -> (Edge a)) es
 
-shortenNode:: [Tag] -> [Node]
-shortenNode ts = map nodeTransform $ ts
-
-nodeTransform:: Tag -> Node
-nodeTransform(Tag _ as _) = Node $ (filter (isAllowed ["id"]) as)
-
-shortenEdge:: [Tag] -> [Edge]
-shortenEdge ts = map edgeTransform $ ts
-
-edgeTransform:: Tag -> Edge
-edgeTransform(Tag _ as _) = Edge $ (filter (isAllowed ["id", "source", "target"]) as)
-
-isAllowed:: [String] -> Attribute -> Bool
-isAllowed ss (Attribute name, val) = name `elem` ss
+filterAtt:: Tag -> Tag
+filterAtt (Tag str as _) =
+  if str == "node" then Tag str (filter (\(Attribute (name, _))-> name == "id") as) []
+  else Tag str (filter (\(Attribute (name, _))-> elem name ["id", "target", "source"]) as) []
