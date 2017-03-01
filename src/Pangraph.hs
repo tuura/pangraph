75cfd535eeab6ce8c314f91bc450cfd15651a42b
Joe <notwhereitype@gmail.com>
2017-03-01 19:39:47 +0000

    Purged old code entirely, rewrote types in Pangraph.hs as well as getter interface

--- src/Pangraph.hs
+++ src/Pangraph.hs
@@ -1,8 +1,34 @@
-module Pangraph
-(
-
+module Pangraph (
+Pangraph,
+Node,
+Edge,
+Att,
+getAtt,
+getNodes,
+getEdges
+getBytestring
 ) where
 
-data Graph = Graph [Node ::Node] [Arc ::Arc]
-data Attribute = Node [ByteString]
-                | Arc [ByteString]
+import qualified Data.ByteString as BS
+
+data Pangraph = Pangraph [Node] [Edge]
+data Node = Node [Att]
+data Edge = Edge [Att]
+data Att = Att (BS.ByteString, BS.ByteString)
+
+class HasAtt a where
+  getAtt:: a -> [Att]
+
+instance HasAtt Node where
+  getAtt (Node a) = a
+instance HasAtt Edge where
+  getAtt (Edge a) = a
+
+getNodes::Pangraph -> [Node]
+getNodes (Pangraph ns _)= ns
+
+getEdges::Pangraph -> [Edge]
+getEdges (Pangraph _ es)= es
+
+getBytestring::Att -> (BS.ByteString, BS.ByteString)
+getBS (Att a) = a
