aad0b01711895c2b1916cc2f7156703b3c0fd655
Joe <Joe>
2017-07-18 16:32:01 +0100

    Fixed git issues, last few commits contain details of this commit

--- src/Pangraph/Internal/Graph.hs
+++ src/Pangraph/Internal/Graph.hs
@@ -8,14 +8,13 @@ Vertex,
 Attribute, -- A type alias for (Key, Value)
 Key,
 Value,
-Identifier,
+VertexID,
+EdgeID,
 
 -- Constructors
 makePangraph,
 makeEdge,
 makeVertex,
--- makeKey,
--- makeValue,
 
 -- Pangraph Getters
 edges,
@@ -30,7 +29,6 @@ vertexAttributes,
 edgeID,
 vertexID,
 
-<<<<<<< HEAD
 -- Operators
 insertVertex,
 addEdge,
@@ -41,74 +39,55 @@ vertexByID,
 edgeByID,
 vertexToAssocList,
 edgeToAssocList
-=======
--- Getters on Attributes
--- key,
--- value
->>>>>>> parent of 2520073... VHDL working again, most likely the list of nodes it used was reordered in conversion to and from maps. Moved helper functions into the internal module. Added functions like  and
+
 ) where
 
 import Data.Map.Strict (Map)
 import Data.List (intercalate)
-import Data.ByteString.Char8 (pack)
+import Data.ByteString.Char8 (pack, unpack)
 import qualified Data.Map.Strict                  as Map
 import qualified Data.ByteString                  as BS
 
 data Pangraph = Pangraph
-  { vertices' :: Map Identifier Vertex
-  , edges' :: Map Identifier Edge
+  { vertices' :: Map VertexID Vertex
+  , edges' :: Map EdgeID Edge
+  , nextEdge' :: EdgeID
   } deriving (Eq)
 data Vertex = Vertex
-  { vertexID' :: Identifier
+  { vertexID' :: VertexID
   , vertexAttributes' :: [Attribute]
   } deriving (Eq)
 data Edge = Edge
-  { edgeID' :: Maybe Identifier
+  { edgeID' :: Maybe EdgeID
   , edgeAttributes' :: [Attribute]
   , endpoints' :: (Vertex, Vertex)
   } deriving (Eq)
 
-type Identifier = BS.ByteString
+type VertexID = BS.ByteString
+type EdgeID = Int
 type Attribute = (Key, Value)
 type Key = BS.ByteString
 type Value = BS.ByteString
 
--- newtype Key = Key BS.ByteString deriving (Eq, Ord, Show)
--- newtype Value = Value BS.ByteString deriving (Eq, Ord, Show)
-
 -- List based constructors
 
 makePangraph :: [Vertex] -> [Edge] -> Pangraph
-makePangraph vs es = Pangraph (Map.fromList indexVertices) (Map.fromList indexEdges)
+makePangraph vs es = Pangraph (Map.fromList indexVertices) edgeMap (1 + Map.size edgeMap)
   where
-    indexVertices :: [(Identifier, Vertex)]
+    edgeMap :: Map EdgeID Edge
+    edgeMap = Map.fromList indexEdges
+    indexVertices :: [(VertexID, Vertex)]
     indexVertices = zip (map vertexID vs) vs
-    indexEdges :: [(Identifier, Edge)]
-    indexEdges = map (\ (i, (Edge _ as a)) -> (i, (Edge (Just i) as a ))) $ zip indexList es
-    indexList = (map (pack . show) [0..]) :: [Identifier]
+    indexEdges :: [(EdgeID, Edge)]
+    indexEdges = map (\ (i, (Edge _ as a)) -> (i, (Edge (Just i) as a ))) $ zip [0..] es
 
--- The nothing nothing occupies the Identifier field.
+-- The nothing occupies the Identifier field until they are all gathered an IDed.
+
 makeEdge :: [Attribute] -> (Vertex, Vertex) -> Edge
 makeEdge = Edge Nothing
 
-makeVertex :: Identifier -> [Attribute] -> Vertex
+makeVertex :: VertexID -> [Attribute] -> Vertex
 makeVertex = Vertex
-
--- Other Constructors
-
--- makeKey :: BS.ByteString -> Key
--- makeKey = Key
---
--- makeValue :: BS.ByteString -> Value
--- makeValue = Value
-
--- Getters
-
-mapEdges :: Pangraph -> Map Identifier Edge
-mapEdges p = edges' p
-
-mapVertices :: Pangraph -> Map Identifier Vertex
-mapVertices p = vertices' p
 
 edges :: Pangraph -> [Edge]
 edges p = map snd $ Map.toList $ edges' p
@@ -116,24 +95,26 @@ edges p = map snd $ Map.toList $ edges' p
 vertices :: Pangraph -> [Vertex]
 vertices p = map snd $ Map.toList $ vertices' p
 
+mapEdges :: Pangraph -> Map EdgeID Edge
+mapEdges = edges'
+
+mapVertices :: Pangraph -> Map VertexID Vertex
+mapVertices = vertices'
+
 edgeAttributes :: Edge -> [Attribute]
 edgeAttributes = edgeAttributes'
 
 vertexAttributes :: Vertex -> [Attribute]
 vertexAttributes = vertexAttributes'
-edgeID :: Edge -> Identifier
 
-vertexID :: Vertex -> Identifier
+vertexID :: Vertex -> VertexID
 vertexID = vertexID'
 
+edgeID :: Edge -> EdgeID
 edgeID edge =
   case edgeID' edge of
     Just a -> a
     Nothing -> error $ "Fatal: Edge missing ID, " ++ show edge
-
-<<<<<<< HEAD
-vertexID :: Vertex -> VertexID
-vertexID = vertexID'
 
 -- Operations on Pangraph
 
@@ -172,16 +153,12 @@ edgeByID key p = Map.lookup key (mapEdges p)
 
 vertexToAssocList :: [Vertex] -> [(VertexID, Vertex)]
 vertexToAssocList vs = map (\v -> (vertexID v, v)) vs
-=======
+
+edgeToAssocList :: [Edge] -> [(EdgeID, Edge)]
+edgeToAssocList es = map (\e -> (edgeID e, e)) es
+
 edgeEndpoints :: Edge -> (Vertex, Vertex)
 edgeEndpoints = endpoints'
->>>>>>> parent of 2520073... VHDL working again, most likely the list of nodes it used was reordered in conversion to and from maps. Moved helper functions into the internal module. Added functions like  and
-
--- key :: Key -> BS.ByteString
--- key (Key a) = a
---
--- value :: Value -> BS.ByteString
--- value (Value a) = a
 
 instance Show Pangraph where
   show p = "Pangraph " ++ show (vertices p) ++ " " ++ show (edges p)
@@ -191,6 +168,6 @@ instance Show Vertex where
 
 instance Show Edge where
   show e@(Edge _ as ends) = intercalate " " ["Edge", show (edgeID e) ,show as] -- show tupleID]
-    where
-      tupleID :: (Identifier, Identifier)
-      tupleID = (vertexID' $ fst ends ,vertexID' $ snd ends)
+    -- where
+    --   tupleID :: (Identifier, Identifier)
+    --   tupleID = (vertexID' $ fst ends ,vertexID' $ snd ends)
