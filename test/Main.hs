7668cc5d60f5de90f66dafcf17aee5ee24ae8c73
Joe Scott <baaquackmoo@gmail.com>
2018-06-27 15:44:55 +0100

    Addition of FGL conversion. (#27)
    * Added tests for edgeIDs in Pangraph
    
    * Added conversions to allow access to FGL
    
    * Added a test for FGL

--- test/Main.hs
+++ test/Main.hs
@@ -3,8 +3,10 @@ module Main where
 import GraphML
 import Show
 import Containers
+import TestPangraph
+import FGL
 
 import Test.HUnit
 
 main :: IO Counts
-main = runTestTT $ TestList $ concat [graphmlTests, showTests, containersTests]
+main = (runTestTT . TestList . concat) [containersTests, fglTests, graphmlTests, showTests,  pangraphTests]
