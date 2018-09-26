ecb4305495b5a1f67c3ef890b42c8456b69ba01d
jscott <baaquackmoo@gmail.com>
2018-09-26 11:01:32 +0100

    Include Workcraft parsing in Pangraph.Examples

--- src/Pangraph/Example.hs
+++ src/Pangraph/Example.hs
@@ -1,5 +1,5 @@
 module Pangraph.Example where
-
+    
 import Pangraph.Examples.SampleGraph(smallGraph)
 
 import qualified Pangraph.GraphML.Parser as GraphML
@@ -10,8 +10,15 @@ import qualified Pangraph.Gml.Writer as Gml
 
 import qualified Pangraph.Containers as Containers
 
+import qualified Pangraph.Workcraft.Parser as Workcraft
+
+import qualified Data.ByteString as BS 
+
 main :: IO ()
 main = do 
+    workcraftXML <- BS.readFile "./examples/graphs/model.xml"
+    print $ Workcraft.parse workcraftXML
+    
     -- Serialise and re-parse the graph in to GraphML.
     let Just gGraphML = (GraphML.parse . GraphML.write) smallGraph
     -- Test equality
